ba565fe0fdc32b12a835536775ae1889bb1ca136
Richard <rick110400@gmail.com>
2024-08-27 17:35:56 -0300

    initial commit

---
+++ slides.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function slider() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[currentSlide].style.display = "block";
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+}
+
+setInterval(slider, 2000);
