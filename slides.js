9ce6bf8e37ad2a07806f5c417ee0ef2d727544d6
Richard <rick110400@gmail.com>
2024-09-02 17:34:48 -0300

    navbar sem style

--- slides.js
+++ slides.js
@@ -15,3 +15,17 @@ function slider() {
 }
 
 setInterval(slider, 3000);
+
+
+const icon = document.querySelector('.icon');
+    const menu = document.querySelector('.menu');
+    const span = document.querySelector('.span')
+
+    function toggleNavbar() {
+        menu.classList.toggle('active');
+        icon.classList.toggle('active');
+    }
+
+    document.querySelector('.toggle-btn').addEventListener('click', function () {
+        toggleNavbar();
+    });
